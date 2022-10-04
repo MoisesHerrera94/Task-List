352057b64137a8bb8c820f59a2751c18ac121778
Moises Herrera <moyhr94@gmail.com>
2022-10-03 22:29:56 -0300

    Creando smock de tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar el primer modulo",
+        day: "Agosto 5",
+        reminder: true
+    }, 
+    {
+        id: 2,
+        text: "Hacer Compras",
+        day: "Agosto 6",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Estudiar",
+        day: "Agosto 7",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer un libro",
+        day: "Agosto 8",
+        reminder: false
+    }
+]
